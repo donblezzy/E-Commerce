1f9845b5eb0e68bbdca299ab0e8a7d74a6626288
donblezzy <99478271+donblezzy@users.noreply.github.com>
2024-12-06 16:37:19 +0100

    Update NewProduct.js

--- frontend/src/components/admin/NewProduct.js
+++ frontend/src/components/admin/NewProduct.js
@@ -32,6 +32,7 @@ const NewProduct = () => {
       // eslint-disable-next-line
       navigate("/admin/products")
     }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [error, isSuccess]);
 
   const {name, description, price, category, stock, seller} = product
